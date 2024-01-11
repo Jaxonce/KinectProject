eab4f91274248662a51f146506612c1a47d48d0a
malanone <maxence.lanone@etu.uca.fr>
2024-01-11 10:51:29 +0100

    change csproj for compatibilty with mvvm, and add button on window

--- KinectProject/KinectProject/MainWindow.xaml.cs
+++ KinectProject/KinectProject/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace KinectProject
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
