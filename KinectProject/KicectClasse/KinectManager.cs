0f114a264031ba2b6bdbf2292b011f4e78bdf4ee
malanone <maxence.lanone@etu.uca.fr>
2024-01-11 12:04:11 +0100

    add manager + mvvm for ObservableProperty

---
+++ KinectProject/KicectClasse/KinectManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KicectClasse
+{
+    internal partial class KinectManager: ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(StatusText))]  
+        private bool status;
+        public String StatusText => status ? "Running" : "Kinect Sensor not available";
+
+        public KinectSensor kinectSensor { get; set; }
+
+        public KinectManager()
+        {
+            kinectSensor = KinectSensor.GetDefault();
+            kinectSensor.IsAvailableChanged += KinectSensor_IsAvailableChanged;
+        }
+
+        public void StartSensor()
+        {
+            kinectSensor?.Open();
+        }
+
+        public void StopSensor() {
+            kinectSensor?.Close();
+        }
+
+        private void KinectSensor_IsAvailableChanged(Object sender, EventArgs args) {
+            status = kinectSensor.IsAvailable;
+        }
+
+    }
+}
